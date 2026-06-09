3ed104223e2209bb820f5e401b967e65589a2a6e
Jamaladdin-Hassan <jamaladdinhassan@gmail.com>
2026-06-09 09:27:17 +0300

    remove old

--- 01_Basics_and_DataTypes/input.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() 
-{
-int age;
-printf("\nPlease enter your age: ");
-scanf("%d", &age);
-printf("\nExcellent!\n Next year you will be\n %d years old.\n", age + 1);
-return 0;
-}
--- 01_Basics_and_DataTypes/main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main()
-{
-printf("Hello Sudan, Hello Uganda, Hello Germany!\n");
-return 0;
-}
